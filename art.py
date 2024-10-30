e6685eaacab0c374b5c790aa1e6cadba2c5d627b
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2024-10-29 23:47:58 -0700

    Helpers Complete

---
+++ art.py
@@ -0,0 +1,10 @@
+welcome = '''
+      _ _
+ .-. |.|B|
+ |R|_|M|O|
+ |E|.|A|O| 
+ |A|-|N|K|  
+ |D|.|Y|S|       Welcome to the
+ | |-|.| |  Library Management System!
+""""""""""""""""""""""""""""""""""""""
+'''
---
+++ main.py
@@ -0,0 +1,13 @@
+from art import welcome
+from helpers import clr, hr
+
+clr()
+print(welcome)
+
+print('''
+Main Menu:
+      1. Book Operations
+      2. User Operations
+      3. Author Operations
+      4. Quit
+''')
