323618414436d5c8668fa879576ece81e9346edc
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2024-10-30 18:26:29 -0700

    Borrow Book Complete

--- book_mod.py
+++ book_mod.py
@@ -13,33 +13,79 @@ class Book:
         self.availability = True
         Book.book_library.append(self)
 
-    def get_book(self):
-        print(self.title) 
+    def get_title(self):
+        return self.title
+    
+    def get_author(self):
+        return self.author
+    
+    def get_genre(self):
+        return self.genre
+    
+    def get_publication_date(self):
+        return self.publication_date
+
+    def get_availability(self):
+        return self.availability
     
     def switch_availability(self):
-        availability = self.availability
-        if availability == True:
+        if self.availability == True:
             self.availability = False
         else:
             self.availability = True
 
-    def borrow_book(self):
-        pass
+    @classmethod
+    def display_all_books(cls):
+        clr()
+        print("Books In Library")
+        for book in cls.book_library:
+            hr(50)
+            print(f"Title: {book.get_title()}")
+            print(f"Author: {book.get_author()}")
 
 # ==================== ***** Functions ***** ====================
 
 def add_book():
     '''Creates new book and prints confirmation.'''
+    clr()
+    hr(50)
     try:
-        title = input("Enter Book Title: ")
-        author = input("Enter Book Author: ")
-        genre = input("Enter Book Genre: ")
-        publication_date = input("Enter Book Publication Date: ")
+        title = input("Enter Title: ")
+        author = input("Enter Author: ")
+        genre = input("Enter Genre: ")
+        publication_date = input("Enter Publication Date: ")
 
         new_book = Book(title, author, genre, publication_date)
-        print(f'{new_book.title}: by {new_book.author},\nhas been added to the library.')
+        clr()
+        hr(50)
+        print(f'{new_book.title}: by {new_book.author},\nhas been added to your library.')
     except ValueError:
+        clr()
+        hr(50)
         print("Input is invalid. Try again.")
+
+def display_books():
+    Book.display_all_books()
+
+def borrow_book():
+    clr()
+    hr(50)
+    borrow_title = input("Enter Title: ")
+
+    book_to_borrow = None
+    for book in Book.book_library:
+        if book.get_title() == borrow_title:
+            book_to_borrow = book
+            break
+
+    if book_to_borrow:
+        if book_to_borrow.get_availability() == True:
+            print(f"You've borrowed: {book_to_borrow.get_title()}.")
+            book_to_borrow.switch_availability()
+        else:
+            print(f"Sorry, {borrow_title} is currently unavailable.")
+    else:
+        print(f"{borrow_title} not found in the library.")
 
 def book_menu():
     clr()
@@ -62,13 +108,13 @@ Main Book Menu:
             if book_operation == 1:
                 add_book()
             elif book_operation == 2:
-                Book.borrow_book()
+                borrow_book()
             elif book_operation == 3:
                 pass
             elif book_operation == 4:
                 pass
             elif book_operation == 5:
-                pass
+                display_books()
             elif book_operation == 6:
                 clr()
                 hr(50)
@@ -81,3 +127,4 @@ Main Book Menu:
             clr()
             hr(50)
             print("Input must be a number between 1-6")
+
